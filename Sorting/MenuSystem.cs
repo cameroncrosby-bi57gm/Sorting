04052d25c96136a1c677e706c310f3864f7348cb
cameroncrosby-bi57gm <cameroncrosby111@gmail.com>
2024-10-03 12:02:49 +0100

    Add project files.

---
+++ Sorting/MenuSystem.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Sorting
+{
+    internal class MenuSystem
+    {
+        string callingClass;
+        Dictionary<string, MethodInfo> menuOptions;
+
+        public MenuSystem(string callingClassName)
+        {
+            callingClass = callingClassName;
+
+            menuOptions = new Dictionary<string, MethodInfo>();
+
+            Type thisType = typeof(Environment);
+            AddMenuItem("Exit Application", thisType.GetMethod("Exit"));
+        }
+
+        public void RunForever()
+        {
+            while(true)
+            {
+                Run();
+            }
+        }
+
+        public void Run()
+        {
+            DisplayOptions();
+            GetMenuSelection();
+        }
+
+        void DisplayOptions()
+        {
+            Console.Clear();
+            Console.WriteLine("Please choose from the following options: \n");
+            int i = 0;
+
+            foreach (string menuItem in menuOptions.Keys)
+            {
+                Console.WriteLine("(" + i + ") " + menuItem);
+                i++;
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        void GetMenuSelection() //validation that the user has inputted an integer
+        {
+            string userInput = Console.ReadLine();
+
+            MethodInfo mi;
+
+            if (int.TryParse(userInput, out int integerInput))
+            {
+                if (integerInput == 0)
+                {
+                    mi = menuOptions.ElementAt(integerInput).Value;
+                    mi.Invoke(this, new object[] { 0 });
+                    Console.ReadKey();
+                }
+                else if (integerInput > 0 && integerInput <menuOptions.Count)
+                {
+                    mi = menuOptions.ElementAt(integerInput).Value;
+                    mi.Invoke(this, null);
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        public void AddMenuItem(string menuMessage, MethodInfo methodInfo)
+        {
+            menuOptions.Add(menuMessage, methodInfo);
+        }
+
+        public void AddMenuItem(string menuMessage, string methodName, Type callingType)
+        {
+            MethodInfo methodInfo = callingType.GetMethod(methodName);
+            menuOptions.Add(menuMessage, methodInfo);
+
+        }
+    }
+}
---
+++ Sorting/MergeSort.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    internal class MergeSort
+    {
+        public static void Perform(List<double> listToSort)
+        {
+            if (listToSort.Count <= 1) //checking to see if the list is greater than 1
+                return;
+
+            int mid = listToSort.Count / 2; //halfs the list
+
+            List<double> left = listToSort.GetRange(0, mid);//gets values of first in array, to central
+            List<double> right = listToSort.GetRange(mid, listToSort.Count - mid); //gets the values from middle, to last in array
+
+            Perform(left);
+            Perform(right);
+
+            Merge(listToSort, left, right); //merges the two halves back together
+        }
+
+        private static void Merge(List<double> listToSort, List<double> left, List<double> right) //using comparison to check if each side of the array has 1 or less values 
+        {
+            int leftIndex = 0, rightIndex = 0, sortedIndex = 0;
+
+            while (leftIndex < left.Count && rightIndex < right.Count) //comparing if the index is bigger than the length of the array
+            {
+                if (left[leftIndex] <= right[rightIndex]) //compares the values of the left side to the right
+                {
+                    listToSort[sortedIndex] = left[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    listToSort[sortedIndex] = right[rightIndex];
+                    rightIndex++;
+                }
+                sortedIndex++;
+            }
+
+            while (leftIndex < left.Count)
+            {
+                listToSort[sortedIndex] = left[leftIndex];
+                leftIndex++;
+                sortedIndex++;
+            }
+
+            while (rightIndex < right.Count)
+            {
+                listToSort[sortedIndex] = right[rightIndex];
+                rightIndex++;
+                sortedIndex++;
+            }
+        }
+    }
+}
